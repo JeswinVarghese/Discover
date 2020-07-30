c83d75f68392bbca37e53d9edaa83a09b8d21b81
JeswinVarghese <jeswinmv@gmail.com>
2020-07-30 18:25:16 +0530

    seventh commit

--- app/src/main/java/com/ron/exploreapp/MainActivity.java
+++ app/src/main/java/com/ron/exploreapp/MainActivity.java
@@ -5,7 +5,6 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
-import android.widget.ImageView;
 
 
 public class MainActivity extends AppCompatActivity {
